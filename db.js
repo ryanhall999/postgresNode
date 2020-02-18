241e2477da82d38f5d1ff0aa5586cf0fc8119226
Ryan Hall <51058871+ryanhall999@users.noreply.github.com>
2020-02-17 20:36:36 -0500

    Add files via upload

--- db.js
+++ db.js
@@ -1,4 +1,5 @@
 const { Client } = require("pg");
+const uuid = require("uuid");
 const client = new Client("postgres://localhost/nodepg");
 
 client.connect();
@@ -20,9 +21,8 @@ const sync = async () => {
       date_created TIMESTAMP DEFAULT CURRENT_TIMESTAMP
     );`;
 	await client.query(SQL);
-	const authors = await readAuthors();
-	const articles = await readArticles();
-	console.log(authors, articles);
+	// await deleteAuthor(authors[0].id);
+	// const articles = await createArticle("words", "words words words words");
 };
 
 const readAuthors = async () => {
@@ -31,16 +31,48 @@ const readAuthors = async () => {
 	return response.rows;
 };
 
-const readArticles = async () => {
-	const SQL = `SELECT * FROM articles`;
-	const response = await client.query(SQL);
+const readAuthor = async id => {
+	const SQL = `SELECT * FROM authors WHERE id=$1`;
+	const response = await client.query(SQL, [id]);
 	return response.rows;
 };
+
+const deleteAuthor = async id => {
+	const SQL = `DELETE FROM authors WHERE id=$1`;
+	await client.query(SQL, [id]);
+};
+
+const createAuthors = async (firstName, lastName) => {
+	const SQL = `INSERT INTO authors(id, first_name, last_name) values( $1, $2, $3)`;
+	await client.query(SQL, [uuid(), firstName, lastName]);
+};
+
+const updateAuthor = async author => {
+	const SQL = `UPDATE authors set first_name = $1, last_name = $2 WHERE id=$3`;
+	const response = client.query(SQL, [
+		author.first_name,
+		author.last_name,
+		author.id
+	]);
+};
+
+// const readArticles = async () => {
+// 	const SQL = `SELECT * FROM articles`;
+// 	const response = await client.query(SQL);
+// 	return response.rows;
+// };
+// const createArticle = async (title, body) => {
+// 	const SQL = `INSERT INTO articles(id, title, body) values( $1, $2, $3)`;
+// 	await client.query(SQL, [uuid(), title, body]);
+// };
 
 sync();
 
 module.exports = {
 	sync,
 	readAuthors,
-	readArticles
+	readAuthor,
+	deleteAuthor,
+	createAuthors,
+	updateAuthor
 };
--- server.js
+++ server.js
@@ -2,6 +2,18 @@ const db = require("./db");
 
 db.sync().then(async () => {
 	console.log("synced");
-	console.log(await db.readArticles());
-	console.log(await db.readAuthors());
+	// console.log(await db.readArticles());
+	await db.createAuthors("ryan", "hall");
+	let authors = await db.readAuthors();
+	console.log(authors);
+	// await db.createAuthors("bob", "dog");
+	// authors = await db.readAuthors();
+	// console.log(authors);
+	// await db.deleteAuthor(authors[0].id);
+	// authors = await db.readAuthors();
+	// console.log(authors);
+	// let newAuth = { first_name: "rob", last_name: "hall", id: authors[0].id };
+	// await db.updateAuthor(newAuth);
+	// authors = await db.readAuthors();
+	// console.log(authors);
 });
